282144cbcea73d922b3b333f6e98e7bd9969cb00
Sukhavindersingh <Sukhavindersingh@LAPTOP-PTC9PINO>
2023-09-04 17:00:22 +0530

    added mew controller

---
+++ Controllers/NewsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FullStackAPI.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
